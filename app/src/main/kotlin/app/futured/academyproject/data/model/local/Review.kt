1d68cafb12bbfae346c1f965e46da759a2b708f4
Daniel Rozehnal <514184@mail.muni.cz>
2023-09-11 19:19:08 +0200

    feat: add times to review model

--- app/src/main/kotlin/app/futured/academyproject/data/model/local/Review.kt
+++ app/src/main/kotlin/app/futured/academyproject/data/model/local/Review.kt
@@ -1,8 +1,12 @@
 package app.futured.academyproject.data.model.local
 
+import java.util.Date
+
 data class Review (
     val reviewId: Int,
     val reviewedPlaceId: Int,
     val stars: Int,
     val description: String,
+    val timeCreated: Date,
+    val timeVisited: Date,
 )
